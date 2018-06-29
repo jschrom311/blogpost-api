1e230d6574e4310c6b15da3ea4e4ba304c969e57
GitGud <jschrom311@yahoo.com>
2018-06-29 03:47:14 -0500

    first commit

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const morgan = require("morgan");
+
+const app = express();
+
+app.use(morgan("common"));
+app.use(express.json());
+
+app.listen(process.env.PORT || 8080, () => {
+  console.log(`Your app is listening on port ${process.env.PORT || 8080}`);
+});
